2a4c2e54beb56308c80d278642ae5fe762f8c015
Ali Yorulmaz <aliyrlmz@gmail.com>
2023-02-22 14:15:30 +0300

    End of section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
 
 function App() {
+  const [compaines, setCompanies] = useState([]);
+
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/companies")
+    .then(response => {
+      setCompanies(response.data);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header as='h2' icon ='users' content='Companies'/>
+        <List>
+          {compaines.map((company: any) => (
+            <List.Item key ={company.id}>
+              {company.name}
+            </List.Item>
+          ))}
+        </List>
     </div>
   );
 }
